0133ca2115d8e2a35c341ebddc36e0e334404b5e
Victor <vtorrandell@zertifika.com>
2017-05-28 21:06:48 +0200

    Añadido servidor

---
+++ public/main.js
@@ -0,0 +1,40 @@
+var app = angular.module('angularTodo', []);
+
+function mainController($scope, $http) {  
+    $scope.formData = {};
+
+    // Cuando se cargue la página, pide del API todos los TODOs
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Get.Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo TODO, manda el texto a la API
+    $scope.createTodo = function(){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Create.Error:' + data);
+            });
+    };
+
+    // Borra un TODO despues de checkearlo como acabado
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Delete.Error:' + data);
+            });
+    };
+}
